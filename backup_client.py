55ee0e06d3a0841ba704e59d8db0f0db99a46047
Jigchu <111167590+Jigchu@users.noreply.github.com>
2025-01-21 22:52:08 +0800

    Adding functionality for adding directories and files to backup

--- backup_client.py
+++ backup_client.py
@@ -1,14 +1,12 @@
 import concurrent.futures
 import math
-import os
 import pathlib
 import platform
 from tkinter import *
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 from typing import Literal
 import socket_io as sio
 import socket
-import sys
 
 TIMEOUT_LENGTH = 300
 HOST: str = None
@@ -41,7 +39,9 @@ def non_blocking_executor_shutdown(executor: concurrent.futures.Executor, future
 def __get_size__(path: pathlib.Path):
 	if path.is_dir():
 		return sum(f.stat().st_size for f in path.glob('**/*') if f.is_file())
-	return path.stat().st_size
+	elif path.is_file():
+		return path.stat().st_size
+	return -1
 
 def get_size(path: pathlib.Path, unit: Literal["B", "iB"]="B"):
 	base = 1000 if unit == "B" else 1024
@@ -49,6 +49,8 @@ def get_size(path: pathlib.Path, unit: Literal["B", "iB"]="B"):
 	size = __get_size__(path)
 	if size == 0:
 		return "0 B"
+	elif size < 0:
+		return "N/A"
 	
 	power = int(math.log(size, base))
 	prefix = prefixes[power-1]
@@ -87,10 +89,20 @@ class ClientWindow:
 		self.backup_win = BackupWindow()
 
 
-"""
-This does not come with ScrollBars please do it yourself
-"""
 class DirectoryView:
+	"""
+	Although the class is called "Directory View", it is fully capable of
+	displaying files. Though admittedly, that is not what it was originally
+	designed for but knowing there will be definitely be people who want to
+	just back up one file in a whole directory, I have decided to add that
+	functionality. Therefore, anything with the a name containing the word
+	directory and all its other derivatives are also able to work with paths
+	to files except for its methods. Methods meant to handle both files and
+	directories use the word item due to a lack of a better word.
+
+	DISCLAIMER: This class does not come with ScrollBars, you have to implement
+	that yourself
+	"""
 	def __init__(self, parent: Misc | None = None):
 		self.directories = []
 
@@ -133,15 +145,7 @@ class DirectoryView:
 				message="There are no directories, please check your server."
 			)
 			return
-
-		# Set the root directory
-		root_dir = pathlib.Path(sys.executable).anchor
-		if platform.system() == "Windows":
-			root_dir = root_dir[:2].upper()
-		self.view.insert(
-			"", iid=root_dir, index=0, text=root_dir,
-			values=("", "\u2610")
-		)
+		
 		root.after(1, self.__populate__, 0)
 
 	def __populate__(self, index):
@@ -156,56 +160,43 @@ class DirectoryView:
 			self.directories.remove(directory)
 			root.after(10, self.__populate__, index)
 			return
-		
-		self.add_item(path, selected=True)
+	
+		# Add base node with children if directory
+		self.add_item(path, base=True)
+		if (not path.is_dir() and not path.is_file()):
+			messagebox.showerror(
+				title="Erm",
+				message="What did you give me? How is {path} somehow not a file or directory? I'm confused"
+			)
+
 		root.after(10, self.__populate__, index+1)
 	
-	def add_item(self, path: pathlib.Path, selected: bool=False):
-		posix_path = path.as_posix()
-		path_segments = posix_path.split(sep="/")
+	def add_item(self, path: pathlib.Path, base: bool=False):
+		text = str(path) if base else path.name
+		parent = "" if base else path.parent.as_posix()
+		self.view.insert(parent, iid=path.as_posix(), index="end", text=text, values=(self.update_size(path), "\u2611"))
 
-		# Check if all parents are created
-		for i in range(len(path_segments[:-1])):
-			parent = "/".join(path_segments[:i+1])
-			if not self.view.exists(parent):
-				self.__add_item__(pathlib.Path(parent), type="dir")
-	
 		if path.is_dir():
-			self.__add_item__(path, "dir", selected)
-		else:
-			self.__add_item__(path, "file", selected)
+			self.__add_children__(path)
 
-	def __add_item__(self, path: pathlib.Path, type: Literal["dir", "file"], selected: bool=False):
-		checkbox = "\u2611" if selected else "\u2610"
-		size = get_size(path) if selected else ""
-		posix_path = path.as_posix()
-		path_segments = posix_path.split(sep="/")
-
-		parent_iid = "/".join(path_segments[:-1])
-		self.view.insert(
-			parent_iid, iid=posix_path, index="end", text=path_segments[-1],
-			values=(size, checkbox)
-		)
-
-		if not selected or type == "file":
-			return
-		
+	def __add_children__(self, path: pathlib.Path):
 		path_sep = "\\" if platform.system() == "Windows" else "/"
 		dirpath = ""
 		dirnames = []
 		filenames = []
-
 		for dirpath, dirnames, filenames in path.walk(follow_symlinks=True):
 			break
-		for dirname in dirnames:
-			if self.view.exists(f"{dirpath}{path_sep}{dirname}"):
-				continue
-			root.after(100, self.add_item, pathlib.Path(f"{dirpath}{path_sep}{dirname}"), True)
-		for filename in filenames:
-			if self.view.exists(f"{dirpath}{path_sep}{filename}"):
-				continue
-			root.after(100, self.add_item, pathlib.Path(f"{dirpath}{path_sep}{filename}"), True)
+
+		children = dirnames + filenames
+		for child in children:
+			child_p = pathlib.Path(f"{dirpath}{path_sep}{child}")
+			root.after(10, self.add_item, child_p)
+
 		return
+
+	# Wrapper for get_size but may be used for optimisations later
+	def update_size(self, path: pathlib.Path, unit: Literal["iB", "B"]="B"):
+		return get_size(path, unit)
 
 class BackupWindow:
 	def __init__(self):
@@ -220,27 +211,30 @@ class BackupWindow:
 
 		# TODO: May make dir view and scroll bars in their own frame
 		self.dir_view = DirectoryView(self.mainframe)
-		self.dir_view.view.grid(column=0, row=0, columnspan=4, rowspan=5, sticky=(N, S, E, W), padx=5, pady=3)
+		self.dir_view.view.grid(column=0, row=0, columnspan=4, rowspan=6, sticky=(N, S, E, W), padx=5, pady=3)
 
 		# Setting Scrollbars
 		vert_scroll = ttk.Scrollbar(self.mainframe, orient="vertical", command=self.dir_view.view.yview)
 		hori_scroll = ttk.Scrollbar(self.mainframe, orient="horizontal", command=self.dir_view.view.xview)
 		self.dir_view.view.configure(yscrollcommand=vert_scroll.set, xscrollcommand=hori_scroll.set)
-		vert_scroll.grid(column=4, row=0, rowspan=5, sticky=(N, S, W), padx=(0, 10))
-		hori_scroll.grid(column=0, row=5, columnspan=4, sticky=(W, E, N), pady=(0, 10))
+		vert_scroll.grid(column=4, row=0, rowspan=6, sticky=(N, S, W), padx=(0, 10))
+		hori_scroll.grid(column=0, row=6, columnspan=4, sticky=(W, E, N), pady=(0, 10))
 
 		# Setting up Buttons
-		add_button = ttk.Button(self.mainframe, text="Add", command=self.add_dir)
+		add_button = ttk.Button(self.mainframe, text="Add Folder", command=self.add_dir)
 		add_button.grid(column=5, row=0, sticky=(N, E), padx=5, pady=(5, 3))
 
-		remove_button = ttk.Button(self.mainframe, text="Remove", command=self.remove_dir)
-		remove_button.grid(column=5, row=1, sticky=(N, E), padx=5, pady=3)
+		add_button = ttk.Button(self.mainframe, text="Add File", command=self.add_file)
+		add_button.grid(column=5, row=1, sticky=(N, E), padx=5, pady=(5, 3))
+
+		remove_button = ttk.Button(self.mainframe, text="Remove", command=self.remove_item)
+		remove_button.grid(column=5, row=2, sticky=(N, E), padx=5, pady=3)
 
 		select_button = ttk.Button(self.mainframe, text="Select All", command=self.select_all)
-		select_button.grid(column=5, row=2, sticky=(N, E), padx=5, pady=3)
+		select_button.grid(column=5, row=3, sticky=(N, E), padx=5, pady=3)
 
 		deselect_button = ttk.Button(self.mainframe, text="Deselect All", command=self.deselect_all)
-		deselect_button.grid(column=5, row=3, sticky=(N, E), padx=5, pady=3)
+		deselect_button.grid(column=5, row=4, sticky=(N, E), padx=5, pady=3)
 
 		backup_button = ttk.Button(self.mainframe, text="Backup", command=self.backup)
 		backup_button.grid(column=3, row=7, sticky=(N, W, E), padx=5, pady=(3, 5))
@@ -249,7 +243,7 @@ class BackupWindow:
 		self.mainframe.columnconfigure(index=0, weight=1)
 		self.mainframe.columnconfigure(index=1, weight=1)
 		self.mainframe.columnconfigure(index=2, weight=1)
-		self.mainframe.rowconfigure(index=4, weight=1)
+		self.mainframe.rowconfigure(index=5, weight=1)
 
 		menubar = Menu(root)
 		file_menu = Menu(menubar)
@@ -262,10 +256,39 @@ class BackupWindow:
 		root.after(100, self.dir_view.populate)
 		root.mainloop()
 
+	# TODO: Make sure it updates the server about these changes and also make sure not to add files that are already present
 	def add_dir(self):
-		raise NotImplementedError
+		"""
+		The reason why the title of dialogs can use platform specific terminology
+		but the name of buttons cannot is due to the fact that the word "Directory"
+		is too long and would cause the button to widen which just looks wrong
+		so "Add Folder" is the name of button regardless of platform while the
+		title will change based on whether you are using a Unix OS or Windows
+		"""
+		terminology = "Folder" if platform.system == "Windows" else "Directory"
+		dir = filedialog.askdirectory(
+			parent=self.mainframe,
+			title=f"Add {terminology}",
+			mustexist=True
+		)
 
-	def remove_dir(self):
+		if dir == "":
+			return
+		self.dir_view.directories.append(dir)
+		self.dir_view.add_item(pathlib.Path(dir), base=True)
+
+	def add_file(self):
+		filename = filedialog.askopenfilename(
+			parent=self.mainframe,
+			title="Add File"
+		)
+
+		if filename == "":
+			return
+		self.dir_view.directories.append(filename)
+		self.dir_view.add_item(pathlib.Path(filename), base=True)
+
+	def remove_item(self):
 		raise NotImplementedError
 
 	def select_all(self):
